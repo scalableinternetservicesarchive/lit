3fcb5702058704aa18ab5f0518cec8090f384a22
dmacaraeg <djmmacaraeg@gmail.com>
2020-10-25 03:19:19 -0700

    Modifies the initial state of the tables such that they match the new schema made in Work.ts and Chapter.tsin relation to date tracking

--- server/src/db/migrations/V1.4__Add_work.sql
+++ server/src/db/migrations/V1.4__Add_work.sql
@@ -1,2 +1,1 @@
-insert into `work` (`userID`, `title`, `summary`,`dateCreated`, `dateModified`, `chapterCurrMaxID`) values (1, 'titletest', 'summary test', 'dateCreated', 'dateModified', 1);
-insert into `work` (`userID`, `title`, `summary`,`dateCreated`, `dateModified`, `chapterCurrMaxID`) values (3, 'titletest', 'summary test', 'dateCreated', 'dateModified', 1);
+insert into `work` (`userId`, `title`, `summary`,`chapterCurrMaxID`) values (1, 'titletest', 'summary test', 1);
--- server/src/db/migrations/V1.5__Add_chapter.sql
+++ server/src/db/migrations/V1.5__Add_chapter.sql
@@ -1,1 +1,1 @@
-insert into `chapter` (`workID`, `chapterID`, `userID`, `title`, `text`, `dateCreated`, `dateModified`) values (1, 1, 1,'titletest', 'testtest', 'dateCreated', 'dateModified');
+insert into `chapter` (`workID`, `chapterID`, `userID`, `title`, `text`) values (1, 1, 1,'titletest', 'testtest');
