7fcb7cc367ecd49643962553da8380d8c14ff0b8
EvelynL09 <37158862+EvelynL09@users.noreply.github.com>
2020-11-06 11:19:39 -0800

    Enable editing

--- server/src/db/migrations/V1.5__Add_chapter.sql
+++ server/src/db/migrations/V1.5__Add_chapter.sql
@@ -1,1 +1,1 @@
-insert into `chapter` (`workID`, `chapterID`, `userID`, `title`, `text`) values (1, 1, 1,'titletest', 'testtest');
+insert into `chapter` (`chapterID`, `userID`, `title`, `text`) values (1, 1,'titletest', 'testtest');
