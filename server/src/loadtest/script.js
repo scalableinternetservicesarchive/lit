8719c12ed31ba1c0132a1a1cc4daa58c1597fd09
Lila R <yrung16@gmail.com>
2020-11-23 12:08:39 -0800

    added our loadtests. They're commented out

--- server/src/loadtest/script.js
+++ server/src/loadtest/script.js
@@ -31,8 +31,8 @@ export default function () {
         'Content-Type': 'application/json',
       },
     }
-  )
   // )
+  )
   sleep(1)
   http.get('http://localhost:3000')
 }
@@ -63,3 +63,89 @@ function recordRates(res) {
     rate500.add(1)
   }
 }
+
+// import http from 'k6/http'
+// import { sleep } from 'k6'
+// import { Counter, Rate } from 'k6/metrics'
+
+// export const options = {
+//   scenarios: {
+//     example_scenario: {
+//       // name of the executor to use
+//       executor: 'ramping-arrival-rate',
+//       // common scenario configuration
+//       startRate: '50',
+//       timeUnit: '1s',
+//       // executor-specific configuration
+//       preAllocatedVUs: 50,
+//       maxVUs: 500,
+//       stages: [
+//         { target: 200, duration: '30s' },
+//         { target: 0, duration: '30s' },
+//       ],
+//     },
+//   },
+// }
+
+// export default function () {
+//   recordRates(http.get('http://localhost:3000'), sleep(1))
+// }
+
+// // export default function () {
+// //   // recordRates(
+// //   const homePage = http.post(
+// //     'http://localhost:3000/graphql',
+// //     '{"operationName":"FetchWorks","variables":{},"query":"query FetchWorks {\n  works {\n    id\n    title\n    summary\n    user {\n      name\n      __typename\n    }\n    __typename\n  }\n}\n"}',
+// //     {
+// //       headers: {
+// //         'Content-Type': 'application/json',
+// //       },
+// //     }
+// //   )
+// //   // )
+// // }
+
+//   // const fetchWork = http.get('http://localhost:3000/work/8/0')
+
+//   // const postWork = http.post(
+//   //   'http://localhost:3000/graphql',
+//   //   '{"operationName":"WorkPost","variables":{"workUserIdPost":6,"workTitlePost":"create","workSummaryPost":"createsum"},"query":"mutation WorkPost($workUserIdPost: Int!, $workTitlePost: String!, $workSummaryPost: String!) {\n  createWork(workUserID: $workUserIdPost, workTitle: $workTitlePost, workSummary: $workSummaryPost)\n}\n"}',
+//   //   {
+//   //     headers: {
+//   //       'Content-Type': 'application/json',
+//   //     },
+//   //   }
+//   // )
+
+//   // )
+//   // sleep(1)
+//   // http.get('http://localhost:3000')
+//   // )
+// //
+
+// const count200 = new Counter('status_code_2xx')
+// const count300 = new Counter('status_code_3xx')
+// const count400 = new Counter('status_code_4xx')
+// const count500 = new Counter('status_code_5xx')
+
+// const rate200 = new Rate('rate_status_code_2xx')
+// const rate300 = new Rate('rate_status_code_3xx')
+// const rate400 = new Rate('rate_status_code_4xx')
+// const rate500 = new Rate('rate_status_code_5xx')
+
+// function recordRates(res) {
+//   if (res.status >= 200 && res.status < 300) {
+//     count200.add(1)
+//     rate200.add(1)
+//   } else if (res.status >= 300 && res.status < 400) {
+//     console.log(res.body)
+//     count300.add(1)
+//     rate300.add(1)
+//   } else if (res.status >= 400 && res.status < 500) {
+//     count400.add(1)
+//     rate400.add(1)
+//   } else if (res.status >= 500 && res.status < 600) {
+//     count500.add(1)
+//     rate500.add(1)
+//   }
+// }
